28b43eff431a8e50180bbae18b31a7713db7fc0f
zuoyou <zuoyou1993@hotmail.com>
2020-08-12 17:12:12 +0100

    Order Module

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2020-07-30 15:33:33
 * @Last Modified by:   zuoyou
-* @Last Modified time: 2020-08-06 20:56:09
+* @Last Modified time: 2020-08-12 16:55:13
 */
 const path = require('path');
 
@@ -34,6 +34,9 @@ var config = {
     'list'                : ['./src/page/list/index.js'],
     'detail'              : ['./src/page/detail/index.js'],
     'cart'                : ['./src/page/cart/index.js'],
+    'order-confirm'       : ['./src/page/order-confirm/index.js'],
+    'order-list'          : ['./src/page/order-list/index.js'],
+    'order-detail'        : ['./src/page/order-detail/index.js'],
     'user-login'          : ['./src/page/user-login/index.js'],
     'user-register'       : ['./src/page/user-register/index.js'],
     'user-pass-reset'     : ['./src/page/user-pass-reset/index.js'],
@@ -87,6 +90,9 @@ var config = {
     new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
     new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
     new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
     new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
     new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
     new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
